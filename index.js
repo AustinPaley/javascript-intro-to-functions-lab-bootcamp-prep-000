df1f43d40160de15225dc0a908a3bc9bc30786b7
Austin Paley <austinpaley32@gmail.com>
2018-03-01 17:47:24 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ function logWhisper(string){
   console.log(lowercase)
 }
 
-function sayHitoGrandma(string){
+function sayHiToGrandma(string){
   if (string.toLowerCase() === string)
   return "I can't hear you!"
 }
