95f92687268a2f950c897e82f450ab8ec47a4dc8
Austin Paley <austinpaley32@gmail.com>
2018-03-01 17:45:36 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,3 +5,8 @@ function shout(string){
 function whisper(string){
   return string.toLowerCase();
 }
+
+function logShout(string){
+  var uppercase = string.toUpperCase();
+  console.log(uppercase)
+}
