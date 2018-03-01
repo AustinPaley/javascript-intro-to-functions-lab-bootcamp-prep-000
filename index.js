b7d6a635c11bbbc863db6d1f8c996b293311ea26
Austin Paley <austinpaley32@gmail.com>
2018-03-01 17:48:16 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ function sayHiToGrandma(string){
   if (string.toLowerCase() === string){
   return "I can't hear you!"
 }
-else if (string.toUppercase() === string)
+else if (string.toUppercase() === string){
 return "YES INDEED!"
-
+}
 }
